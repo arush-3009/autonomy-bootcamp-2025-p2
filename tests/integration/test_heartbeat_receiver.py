80f64b5b24181cd97570615738c21378a826b6ba
Arush Handa <handaarush1237@gmail.com>
2026-04-28 15:12:43 -0400

    fix bug

--- tests/integration/test_heartbeat_receiver.py
+++ tests/integration/test_heartbeat_receiver.py
@@ -140,7 +140,7 @@ def main() -> int:
 
     # Read the main queue (worker outputs)
     # Read the main queue (worker outputs)
-    threading.Thread(target=read_queue, args=(output_queue, controller, main_logger)).start()
+    threading.Thread(target=read_queue, args=(output_queue, controller, main_logger), daemon=True).start()
 
     heartbeat_receiver_worker.heartbeat_receiver_worker(connection=connection, output_queue=output_queue, controller=controller)
     # =============================================================================================
